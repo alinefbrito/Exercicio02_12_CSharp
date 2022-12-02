6434e44f21e241874c922c6b8b57991b48fc541c
Aline F. Brito <alinefbrito@gmail.com>
2022-12-02 22:57:52 +0000

    revisão aula 01/12

--- main.cs
+++ main.cs
@@ -2,9 +2,14 @@ using System;
 
 class Program {
   public static void Main (string[] args) {
+    //declara variavel do tipo texto
     string nome;
+
+  //Escreval("texto")
     Console.WriteLine("Digite seu nome");
+    //leia(variavel)
     nome =Console.ReadLine();
+    //Escreval("Texto", variavel)
     Console.WriteLine ("Hello " + nome);
   }
 }
