f02131fb99420b3b6a3a43b06134754222e337e2
Aline F. Brito <alinefbrito@gmail.com>
2022-12-02 22:48:40 +0000

    Initial commit

---
+++ main.cs
@@ -0,0 +1,10 @@
+using System;
+
+class Program {
+  public static void Main (string[] args) {
+    string nome;
+    Console.WriteLine("Digite seu nome");
+    nome =Console.ReadLine();
+    Console.WriteLine ("Hello " + nome);
+  }
+}
