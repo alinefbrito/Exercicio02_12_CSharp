3e7b397d5dfced8353a7c1921efca38550b0cd41
Aline F. Brito <alinefbrito@gmail.com>
2022-12-03 00:06:55 +0000

    Modificado para efetuar o calculo da média

--- main.cs
+++ main.cs
@@ -3,13 +3,35 @@ using System;
 class Program {
   public static void Main (string[] args) {
     //declara variavel do tipo texto
-    string nome;
 
-  //Escreval("texto")
-    Console.WriteLine("Digite seu nome");
+    
+    //criar variaveis para leitura dos dados para calculo média do aluno
+    
+    string nome, entrada;
+    Double n1, n2, media;
+    //Escreval("texto")
+    Console.WriteLine("Digite o nome do aluno");
     //leia(variavel)
-    nome =Console.ReadLine();
+    nome = Console.ReadLine();
+    //solicita q o usuario digite a n1
+    
+    Console.WriteLine("Digite a primeira nota");
+    //recebe a nota 1 na variavel de texto. Toda a entrada é um texto
+    entrada = Console.ReadLine();
+    // converter a entrada em um Double
+    n1 =  Double.Parse(entrada);
+    //repete para a n2
+    Console.WriteLine ("Digite a nota 2");
+    entrada = Console.ReadLine();
+    n2 = Double.Parse(entrada);
+    //calculo a media
+    media = (n1 +n2)/2;
+    Console.WriteLine ("A média do(a) aluno(a) " + nome + " é " +media);
+
+    
     //Escreval("Texto", variavel)
-    Console.WriteLine ("Hello " + nome);
+    //Console.WriteLine ("Hello " + nome);
+
+    
   }
 }
